e915a849d67a38db7e466826ed264ba5dd095cae
tony-tomass <tonynguyen984@gmail.com>
2023-10-05 14:37:15 -0500

    Added permissions and requests for them

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECEIVE_SMS)
+        != PackageManager.PERMISSION_GRANTED) {
+            String[] permission = new String[]{Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this, permission, 101);
+        }
+
     }
 }
