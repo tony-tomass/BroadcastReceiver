c64daf605c700e3d3eefaec3ff7a8292dbfcf16e
tony-tomass <tonynguyen984@gmail.com>
2023-10-25 17:23:16 -0500

    Changed list to work like a queue

--- app/src/main/java/com/example/broadcastreceiver/TickerViewModel.java
+++ app/src/main/java/com/example/broadcastreceiver/TickerViewModel.java
@@ -52,6 +52,7 @@ public class TickerViewModel extends ViewModel {
             //Question for Estaban: Should it be only replacing the sixth entry like
             //the instructions says or should it be acting like a Queue
             //list.removeFirst();
+            //Makes List work like queue instead of only replacing the 6th item in list
             for (int i = 0; i < list.size()-1; i++) {
                 list.set(i, list.get(i+1));
             }
